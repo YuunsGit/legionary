be7fc2318928f787a3e2e7b7aaeb15176a8d6993
Yunus Emre <yekepenek2002@gmail.com>
2023-01-13 23:42:23 +0300

    Gel command added

---
+++ commands/come.js
@@ -0,0 +1,30 @@
+const { Player } = require("discord-player");
+const player = new Player(client);
+
+module.exports = {
+    data: {
+        name: "avatar",
+        description: "He-Man'i müzik çalmaya çağırır.",
+    },
+    execute(interaction) {
+        setTimeout(async () => {
+            const queue = player.createQueue(client.guilds.cache.get("419963388941172737"));
+
+            try {
+                if (!queue.connection) await queue.connect(interaction.member.voice.channel);
+            } catch {
+                queue.destroy();
+                return console.log("Error: Could not join voice channel!");
+            }
+
+            const track = await player
+                .search("https://www.youtube.com/watch?v=jfKfPfyJRdk", {
+                    requestedBy: client.user,
+                })
+                .then((x) => x.tracks[0]);
+            if (!track) return console.log("Error: Lofi stream not found!");
+
+            queue.play(track);
+        }, 2000);
+    },
+};
