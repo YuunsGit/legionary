1119c4b50fdad777f695e24fdc60176127cfc812
Yunus Emre <yekepenek2002@gmail.com>
2023-01-14 00:03:08 +0300

    Hotfix

--- commands/come.js
+++ commands/come.js
@@ -1,14 +1,14 @@
 const { Player } = require("discord-player");
-const player = new Player(client);
 
 module.exports = {
     data: {
-        name: "avatar",
+        name: "gel",
         description: "He-Man'i müzik çalmaya çağırır.",
     },
     execute(interaction) {
+        const player = new Player(interaction.client);
         setTimeout(async () => {
-            const queue = player.createQueue(client.guilds.cache.get("419963388941172737"));
+            const queue = player.createQueue(interaction.client.guilds.cache.get("419963388941172737"));
 
             try {
                 if (!queue.connection) await queue.connect(interaction.member.voice.channel);
@@ -19,7 +19,7 @@ module.exports = {
 
             const track = await player
                 .search("https://www.youtube.com/watch?v=jfKfPfyJRdk", {
-                    requestedBy: client.user,
+                    requestedBy: interaction.client.user,
                 })
                 .then((x) => x.tracks[0]);
             if (!track) return console.log("Error: Lofi stream not found!");
