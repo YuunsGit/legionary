f05f9a8e0e27f01ddb8215b0fbfa7cc71fef2c99
Yunus Emre Kepenek <yunus.kepenek@dt.net.tr>
2023-04-19 00:29:57 +0300

    GPT initial setup

--- events/chatbot.js
+++ events/chatbot.js
@@ -1,38 +1,13 @@
-const smartestchatbot = require("smartestchatbot");
-
-const client = new smartestchatbot.Client(process.env.CHAT_TOKEN);
-
 module.exports = {
-    name: "messageCreate",
-    async execute(message) {
-        if (message.mentions.users.size !== 1 || !message.mentions.users.has("857603715028877323")) return;
-        if (message.author.id == "857603715028877323") return;
+  name: "messageCreate",
+  async execute(message) {
+    console.log(message.content);
 
-        try {
-            const content = message.content.slice(message.content.indexOf(" ") + 1);
-            message.channel.sendTyping();
-            client
-                .chat(
-                    {
-                        message: content,
-                        name: "He-Man",
-                        master: "Yunus Emre",
-                        user: message.author.id,
-                    },
-                    "tr"
-                )
-                .then((reply) => {
-                    message.reply({
-                        content: reply,
-                        allowedMentions: { repliedUser: true },
-                    });
-                })
-                .catch((e) => {
-                    console.log(e);
-                });
-            message.channel.sendTyping();
-        } catch (e) {
-            console.log(e);
-        }
-    },
+    if (
+      message.mentions.users.size !== 1 ||
+      !message.mentions.users.has("857603715028877323")
+    )
+      return;
+    if (message.author.id == "857603715028877323") return;
+  },
 };
